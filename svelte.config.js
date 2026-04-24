778e937ff0342ed6384464fe92eedfcd09f5699e
Lumivia <admin@lumivia.app>
2026-04-24 09:19:40 -0600

    Change Svelte adapter to Cloudflare and update config

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	compilerOptions: {
-		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
-	},
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+    compilerOptions: {
+        // Forzamos runes excepto en node_modules
+        runes: ({ filename }) =>
+            filename.split(/[/\\]/).includes('node_modules') ? undefined : true
+    },
+    kit: {
+        adapter: adapter({
+            // Opcional: si quieres usar funciones de Cloudflare
+            routes: {
+                include: ['/*'],
+                exclude: ['<all>']
+            }
+        })
+    }
 };
 
 export default config;
