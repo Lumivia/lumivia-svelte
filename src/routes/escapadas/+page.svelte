0705c4fd3f6292da852f3491b8afd1b6d4770b03
Lumivia <admin@lumivia.app>
2026-05-06 10:56:23 -0600

    Add data-sveltekit-reload attribute to link

--- src/routes/escapadas/+page.svelte
+++ src/routes/escapadas/+page.svelte
@@ -149,7 +149,7 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-16 flex items-center justify-between w-full">
       
       <div class="flex items-center gap-4">
-        <a href={`/paises/${paisActual.toLowerCase()}`} class="text-gray-400 hover:text-lumiDark transition-colors cursor-pointer flex items-center" title="Volver a los destinos">
+        <a href={`/paises/${paisActual.toLowerCase()}`} data-sveltekit-reload class="text-gray-400 hover:text-lumiDark transition-colors cursor-pointer flex items-center" title="Volver a los destinos">
           <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" /></svg>
         </a>
         <span class="text-2xl font-extrabold tracking-tighter text-lumiDark flex items-center">
